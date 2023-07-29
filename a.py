35562c29e1f71d24dc95ec5591e55ee7daa89ff2
walidsoufiani <140754926+walidsoufiani@users.noreply.github.com>
2023-07-29 09:49:07 +0100

    Update a.py

--- a.py
+++ a.py
@@ -3,8 +3,10 @@ import requests
 import os
 import base64
 from streamlit_option_menu import option_menu
-import fitz  
-import PyMuPDF
+import streamlit as st
+import pdf2image
+import os
+from io import BytesIO
 
 
 def save_pdf_from_url(url, file_path):
@@ -19,29 +21,18 @@ def get_download_link(file_path):
     href = f'<a href="data:application/pdf;base64,{b64}" download> Télécharger le fichier PDF </a>'
     return href
 
-def convert_pdf_to_images(pdf_file):
-    images = []
-    pdf_document = fitz.open("pdf", pdf_file.read())
-    for page_number in range(pdf_document.page_count):
-        page = pdf_document.load_page(page_number)
-        image_list = page.get_images(full=True)
-        for img_number, img in enumerate(image_list):
-            images.append(img[0].get_pixmap())
 
-    pdf_document.close()
-    return images
 
 st.title("URL to PDF Downloader")
 st.write("Entrez l'URL du PDF que vous souhaitez télécharger et obtenir le lien de téléchargement.")
 
-def main():
     # 1. as sidebar menu
-    with st.sidebar:
+with st.sidebar:
         sidebar_selection = option_menu(" Menu", ['Home','URL','pdf2_image','Layout parser','Extract Table','Question Answering','footprint','Chatboot','Upload json file','Settings','contact' ], 
             icons=['house','file-earmark-pdf-fill','bi bi-file-earmark-image','bi bi-columns-gap','table','bi bi-patch-question-fill','bi bi-app','bi bi-chat-left-dots-fill','bi bi-filetype-json','gear','bi bi-person-lines-fill'], menu_icon="cast", default_index=1)
         sidebar_selection
 
-    if sidebar_selection == 'URL':    
+if sidebar_selection == 'URL':    
         # Saisie de l'URL du PDF
         pdf_url = st.text_input("URL du PDF")
 
@@ -63,65 +54,26 @@ def main():
                 st.error("Une erreur s'est produite lors du téléchargement du PDF.")
                 st.error(e)
 
-    if sidebar_selection == 'pdf2_image':
-        # Titre de l'application
-        st.title("Conversion PDF en images")
+if sidebar_selection == 'pdf2_image':
+        pdf_uploaded = st.file_uploader("Select a file", type="pdf")
+        button = st.button("Confirm")
+        image_down = []
+        st.write("test1")
+        if button and pdf_uploaded is not None:
+            st.write("test2")
+            if pdf_uploaded.type == "application/pdf":
+                st.write("test3")
+                images = pdf2image.convert_from_bytes(pdf_uploaded.read())
+                for i, page in enumerate(images):
+                    st.write(i)
+                    st.write(page)
+                    st.image(page, use_column_width=True)
+                    img = page
+                    buf = BytesIO()
+                    img.save(buf, format="JPEG")
+                    byte_im = buf.getvalue()
+                    st.download_button("Download", data=byte_im, file_name=f"Image_{i}.png")
 
-        # Afficher le bouton de téléchargement de fichier PDF
-        uploaded_file = st.file_uploader("Téléchargez un fichier PDF", type="pdf")
 
-        # Vérifier si un fichier a été téléchargé
-        if uploaded_file is not None:
-            # Convertir le PDF en images
-            images = convert_pdf_to_images(uploaded_file)
 
-            # Afficher les images en rangées sur Streamlit
-            row_num = len(images) // 3 + 1
-            selected_images = []
-
-            # Afficher le choix pour sélectionner une seule image ou toutes les images
-            choice = st.radio("Choisissez une option :", options=["Choisir une image", "Choisir toutes les images"])
-
-            if choice == "Choisir une image":
-                for row in range(row_num):
-                    columns = st.columns(3)
-                    for i, column in enumerate(columns):
-                        index = row * 3 + i
-                        if index < len(images):
-                            # Afficher l'image et la case à cocher correspondante
-                            image = images[index]
-                            checkbox = column.checkbox(label="", key=f"checkbox_{index}")
-                            if checkbox:
-                                selected_images.append(image)
-                            column.image(image, use_column_width=True)
-
-            else:  # Choisir toutes les images
-                selected_images = images
-
-                for row in range(row_num):
-                    columns = st.columns(3)
-                    for i, column in enumerate(columns):
-                        index = row * 3 + i
-                        if index < len(images):
-                            # Afficher l'image
-                            column.image(images[index], use_column_width=True)
-
-            # Bouton pour sauvegarder les images sélectionnées
-            if len(selected_images) > 0:
-                st.write("---")
-                save_directory = st.text_input("Chemin de sauvegarde des images", value="chemin/vers/repertoire")
-                if choice == "Choisir toutes les images":
-                    if st.button("Sauvegarder toutes les images"):
-                        for i, image in enumerate(images):
-                            image_path = os.path.join(save_directory, f"image_{i}.png")
-                            image.save(image_path, "PNG")
-                        st.success("Toutes les images sauvegardées avec succès!")
-                else:
-                    if st.button("Sauvegarder les images sélectionnées"):
-                        for i, image in enumerate(selected_images):
-                            image_path = os.path.join(save_directory, f"image_{i}.png")
-                            image.save(image_path, "PNG")
-                        st.success("Images sauvegardées avec succès!")
-
-if __name__ == "__main__":
-    main()
+   
