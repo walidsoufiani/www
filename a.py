080cc8320aeb18d8ad4a4baab7b65e610ccfdd8b
walidsoufiani <140754926+walidsoufiani@users.noreply.github.com>
2023-07-29 11:00:51 +0100

    Update a.py

--- a.py
+++ a.py
@@ -1,79 +1,38 @@
 import streamlit as st
-import requests
-import os
-import base64
-from streamlit_option_menu import option_menu
-import streamlit as st
 import pdf2image
-import os
-from io import BytesIO
+import zipfile
+import io
 
+st.set_page_config(
+    page_title="PDF to PNG",
+    layout="wide"
+)
+st.markdown("# :spiral_note_pad: Tom's :blue[PDF] to :red[PNG] Converter :frame_with_picture:")
+st.markdown("---")
 
-def save_pdf_from_url(url, file_path):
-    response = requests.get(url)
-    with open(file_path, 'wb') as f:
-        f.write(response.content)
+pdf_uploaded = st.file_uploader("Select a file", type="pdf")
+top_container = st.empty()
 
-def get_download_link(file_path):
-    with open(file_path, 'rb') as f:
-        data = f.read()
-    b64 = base64.b64encode(data).decode('utf-8')
-    href = f'<a href="data:application/pdf;base64,{b64}" download> Télécharger le fichier PDF </a>'
-    return href
+col1, col2, col3 = st.columns([1, 1, 4])
+with top_container, col1:
+    button = st.button("Confirm", type='primary')
 
+st.markdown("---")
+if button and pdf_uploaded is not None:
+    if pdf_uploaded.type == "application/pdf":
+        images = pdf2image.convert_from_bytes(pdf_uploaded.read())
+        file = io.BytesIO()
+        with zipfile.ZipFile(file, 'w') as z:
+            for i, page in enumerate(images):
+                st.image(page, use_column_width=True)
+                img = page
+                buf = io.BytesIO()
+                img.save(buf, format="png")
+                byte_im = buf.getvalue()
+                z.writestr(f'image_{i}.png', byte_im)
+        z.close()
+        file.seek(0)
+        zip_contents = file.getvalue()
 
-
-st.title("URL to PDF Downloader")
-st.write("Entrez l'URL du PDF que vous souhaitez télécharger et obtenir le lien de téléchargement.")
-
-    # 1. as sidebar menu
-with st.sidebar:
-        sidebar_selection = option_menu(" Menu", ['Home','URL','pdf2_image','Layout parser','Extract Table','Question Answering','footprint','Chatboot','Upload json file','Settings','contact' ], 
-            icons=['house','file-earmark-pdf-fill','bi bi-file-earmark-image','bi bi-columns-gap','table','bi bi-patch-question-fill','bi bi-app','bi bi-chat-left-dots-fill','bi bi-filetype-json','gear','bi bi-person-lines-fill'], menu_icon="cast", default_index=1)
-        sidebar_selection
-
-if sidebar_selection == 'URL':    
-        # Saisie de l'URL du PDF
-        pdf_url = st.text_input("URL du PDF")
-
-        if pdf_url and st.button("Télécharger"):
-            try:
-                # Récupérer le nom du fichier à partir de l'URL et ajouter l'extension .pdf
-                file_name = pdf_url.split('/')[-1] + ".pdf"
-                # Chemin d'accès complet pour enregistrer le PDF
-                folder_path = "downloads"
-                os.makedirs(folder_path, exist_ok=True)
-                file_path = os.path.join(folder_path, file_name)
-                # Télécharger le PDF depuis l'URL
-                save_pdf_from_url(pdf_url, file_path)
-                st.success(f"Le PDF a été téléchargé avec succès !")
-                st.info("Cliquez sur le lien ci-dessous pour télécharger le fichier.")
-                # Afficher le lien de téléchargement
-                st.markdown(get_download_link(file_path), unsafe_allow_html=True)
-            except Exception as e:
-                st.error("Une erreur s'est produite lors du téléchargement du PDF.")
-                st.error(e)
-
-if sidebar_selection == 'pdf2_image':
-        pdf_uploaded = st.file_uploader("Select a file", type="pdf")
-        button = st.button("Confirm")
-        image_down = []
-        st.write("test1")
-        if button and pdf_uploaded is not None:
-            st.write("test2")
-            if pdf_uploaded.type == "application/pdf":
-                st.write("test3")
-                images = pdf2image.convert_from_bytes(pdf_uploaded.read())
-                for i, page in enumerate(images):
-                    st.write(i)
-                    st.write(page)
-                    st.image(page, use_column_width=True)
-                    img = page
-                    buf = BytesIO()
-                    img.save(buf, format="JPEG")
-                    byte_im = buf.getvalue()
-                    st.download_button("Download", data=byte_im, file_name=f"Image_{i}.png")
-
-
-
-   
+        with top_container, col2:
+            download = st.download_button("Download", data=zip_contents, file_name=f"Images.zip")
